40cd3dfa53138c3f8f5c695d04f5c1af37ebb217
maxim1317 <snesarev1317@gmail.com>
2018-03-12 15:26:15 +0300

    commented generator.py

--- src/generator.py
+++ src/generator.py
@@ -4,8 +4,14 @@ import random
 
 
 def generator(M, N, offset):
+    """ Takes M (rectangle size), N (number of circles) and
+        offset to get coordinates right
+        And generates coordinates of circle. Nothing fancy.
+        Returns list of points """
+
     circleList = []
     out = open('gens/circlelist.gen', 'w')
+
     for i in range(N):
         circle = [random.uniform(0, M) + offset, random.uniform(0, M) + offset]
         circleList.append(circle)
